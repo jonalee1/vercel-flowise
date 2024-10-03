d4e3e09fa913a81c70ba5c60c6588c9e13cb396b
Jonathan Lee <jlee2@laverne.edu>
2024-10-03 09:48:39 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import { FullPageChat } from "flowise-embed-react";
+
+const App = () => {
+    return (
+        <FullPageChat
+            chatflowid="45360bac-c338-48ef-85e9-b372655f917d"
+            apiHost="https://i37gbon1.rcsrv.net"
+        />
+    );
+};
+
+export default App;
